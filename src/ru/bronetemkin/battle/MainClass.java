c6215f2616a70342ca5c8988e422e8ade6e17a9e
Bronetemkin <nekit1690@gmail.com>
2016-09-26 17:28:28 +0300

    Added some methods

--- src/ru/bronetemkin/battle/MainClass.java
+++ src/ru/bronetemkin/battle/MainClass.java
@@ -4,4 +4,21 @@ package ru.bronetemkin.battle;
  * Created by Bronetemkin on 26.09.2016.
  */
 public class MainClass {
+
+    public void start(){
+
+    }
+
+    public void stop(){
+
+    }
+
+    public void pause(){
+
+    }
+
+    public void resume(){
+
+    }
+
 }
